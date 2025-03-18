065f014f71a1c9a0ff948e4e2c9417d27e5e581f
Rasakul13 <39884629+Rasakul13@users.noreply.github.com>
2025-03-18 16:13:03 +0100

    refactor: changed filter menu

--- m-hub-frontend/src/app/components/filter-menu/filter-menu.component.ts
+++ m-hub-frontend/src/app/components/filter-menu/filter-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-filter-menu',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './filter-menu.component.html',
   styleUrl: './filter-menu.component.scss'
 })
