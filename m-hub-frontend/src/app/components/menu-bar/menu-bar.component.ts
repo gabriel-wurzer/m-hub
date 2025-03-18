d969abdfc406a8f3ac7b65efd8c959ba628095da
Rasakul13 <39884629+Rasakul13@users.noreply.github.com>
2025-03-18 13:44:26 +0100

    style: added styling for navigation rail

--- m-hub-frontend/src/app/components/menu-bar/menu-bar.component.ts
+++ m-hub-frontend/src/app/components/menu-bar/menu-bar.component.ts
@@ -1,25 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-menu-bar',
   standalone: true,
-  imports: [ RouterOutlet, CommonModule, MatToolbarModule,MatSidenavModule, MatListModule, MatIconModule, MatButtonModule],
+  imports: [ RouterOutlet, CommonModule, MatToolbarModule,MatSidenavModule, MatListModule, MatIconModule, MatButtonModule, MatDividerModule],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.scss'
 })
 export class MenuBarComponent implements OnInit {
 
+  currentRoute: string = '/map';
+
   isMobile = false;
 
-  constructor(private router: Router, private breakpointObserver: BreakpointObserver) {}
+  constructor(private router: Router, private breakpointObserver: BreakpointObserver) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Example: '/map', '/hub', '/data'
+        this.currentRoute = event.urlAfterRedirects;
+      }
+    });
+  }
 
   ngOnInit() {
     this.breakpointObserver.observe(['(max-width: 1199px)']).subscribe(result => {
@@ -27,21 +37,25 @@ export class MenuBarComponent implements OnInit {
     });
   }
 
-  onMenuClick(menuName: string) {
-    switch (menuName) {
-      case 'map':
-        this.router.navigate(['/map']);
-        break;
-      case 'hub':
-        this.router.navigate(['/hub']);
-        break;
-      case 'data':
-        this.router.navigate(['/data']);
-        break;
-      default:
-        console.warn('Unknown route:', menuName);
-        break;
-    }
+  // onMenuClick(menuName: string) {
+  //   switch (menuName) {
+  //     case 'map':
+  //       this.router.navigate(['/map']);
+  //       break;
+  //     case 'hub':
+  //       this.router.navigate(['/hub']);
+  //       break;
+  //     case 'data':
+  //       this.router.navigate(['/data']);
+  //       break;
+  //     default:
+  //       console.warn('Unknown route:', menuName);
+  //       break;
+  //   }
+  // }
+
+  onMenuClick(route: string): void {
+    this.router.navigate([`/${route}`]);
   }
 
   onProfileClick() {
