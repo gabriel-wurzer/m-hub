4829285bd612de5a31b9174a0575c506b026e345
Rasakul13 <39884629+Rasakul13@users.noreply.github.com>
2025-04-08 15:11:18 +0200

    feat: add service request functions for fetching building/buildingPart by their id

---
+++ m-hub-frontend/src/app/services/building-part/building-part.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BuildingPart } from '../../models/building-part';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BuildingPartService {
+
+  private apiUrl = 'http://localhost:1880/api/building-part'; // Node-RED endpoint (development)
+
+  constructor(private http: HttpClient) { }
+
+  getBuildingPartById(buildingPartId: string): Observable<BuildingPart> {
+    return this.http.get<BuildingPart>(`${this.apiUrl}/${buildingPartId}`);
+  } 
+}
--- m-hub-frontend/src/app/services/building/building.service.ts
+++ m-hub-frontend/src/app/services/building/building.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { BuildingPart } from '../../models/building-part';
+import { Building } from '../../models/building';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,14 @@ export class BuildingService {
   private apiUrl = 'http://localhost:1880/api/building'; // Node-RED endpoint (development)
 
   constructor(private http: HttpClient) { }
+
+  getBuildingPartById(buildingPartId: string): Observable<BuildingPart> {
+    return this.http.get<BuildingPart>(`${this.apiUrl}/${buildingPartId}`);
+  } 
+
+  getBuildingById(buildingId: number): Observable<Building> {
+    return this.http.get<Building>(`${this.apiUrl}/${buildingId}`);
+  } 
 
   getDocumentsByBuilding(buildingId: number): Observable<BuildingPart[]> {
     return this.http.get<BuildingPart[]>(`${this.apiUrl}/${buildingId}/documents`);
