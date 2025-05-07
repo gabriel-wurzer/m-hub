79b4caea4098a04a6178d64277ad33954d4f3bec
Rasakul13 <39884629+Rasakul13@users.noreply.github.com>
2025-05-07 10:23:04 +0200

    refactor: add new building icons in black and theme primary color

--- m-hub-frontend/src/app/components/user-data/user-data.component.ts
+++ m-hub-frontend/src/app/components/user-data/user-data.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { UserService } from '../../services/user/user.service';
+
 
 @Component({
   selector: 'app-user-data',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatDividerModule, MatListModule, MatProgressSpinnerModule],
   templateUrl: './user-data.component.html',
   styleUrl: './user-data.component.scss'
 })
-export class UserDataComponent {
+export class UserDataComponent implements OnInit {
+
+  userId = "c3e5b0fc-cc48-4a6f-8e27-135b6d3a1b71";
+  buildingIds: number[] = [];
+
+  isLoading = false; 
+  errorMessage = '';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    if(!this.userId) return;
+
+    this.loadBuildings();
+  }
+
+  private loadBuildings(): void {
+    this.isLoading = true;
+    this.userService.getBuildingsByUser(this.userId).subscribe({
+      next: (ids) => {
+        this.buildingIds = ids;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.errorMessage = 'Failed to load buildings.';
+        this.isLoading = false;
+        console.error(error);
+      }
+    });
+  }
 
 }
