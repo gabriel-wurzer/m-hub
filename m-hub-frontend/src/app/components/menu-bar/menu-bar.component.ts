a200dcbcb17777c576f92fce1224785ceeefef83
Rasakul13 <39884629+Rasakul13@users.noreply.github.com>
2025-06-17 16:00:55 +0200

    feat: add tooltip for menu bar buttons

--- m-hub-frontend/src/app/components/menu-bar/menu-bar.component.ts
+++ m-hub-frontend/src/app/components/menu-bar/menu-bar.component.ts
@@ -9,11 +9,23 @@ import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-menu-bar',
   standalone: true,
-  imports: [ RouterOutlet, CommonModule, MatToolbarModule, MatMenuModule, MatSidenavModule, MatListModule, MatIconModule, MatButtonModule, MatDividerModule],
+  imports: [ 
+    RouterOutlet, 
+    CommonModule, 
+    MatToolbarModule, 
+    MatMenuModule, 
+    MatSidenavModule, 
+    MatListModule, 
+    MatIconModule, 
+    MatButtonModule, 
+    MatDividerModule,
+    MatTooltipModule
+  ],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.scss'
 })
