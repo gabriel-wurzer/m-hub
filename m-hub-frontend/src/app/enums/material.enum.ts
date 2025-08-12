9e7ebb8977900efe4ac6144e1b0b550d6f85deec
Rasakul13 <39884629+Rasakul13@users.noreply.github.com>
2025-08-12 13:09:03 +0200

    feat: add enum for building part materials

---
+++ m-hub-frontend/src/app/enums/material.enum.ts
@@ -0,0 +1,36 @@
+/**
+ * Enum for supported mataterials.
+ */
+export enum Material {
+  mat_1 = 'Ziegel',
+  mat_2 = 'mat_örtel',
+  mat_3 = 'Beton',
+  mat_4 = 'Blähbeton',
+  mat_5 = 'Estrich',
+  mat_6 = 'Putz',
+  mat_7 = 'Stahl',
+  mat_8 = 'Heraklith',
+  mat_9 = 'Fliesen',
+  mat_10 = 'Fliesenkleber',
+  mat_11 = 'Rigips',
+  mat_12 = 'Holz',
+  mat_13 = 'Keramat_ik',
+  mat_14 = 'Bitumat_en',
+  mat_15 = 'Glas',
+  mat_16 = 'mat_ineralwolle',
+  mat_17 = 'Styropor',
+  mat_18 = 'PVC',
+  mat_19 = 'Kupfer',
+  mat_20 = 'Alumat_iniumat_',
+  mat_21 = 'Eternit',
+  mat_22 = 'Schlacke',
+  mat_23 = 'Blei',
+  mat_24 = 'Diverse Kunststoffe',
+  mat_25 = 'Stroh',
+  mat_26 = 'Naturstein',
+  mat_27 = 'Papier',
+  mat_28 = 'mat_essing',
+  mat_29 = 'Steinzeug',
+  mat_30 = 'Linoleumat_'
+} 
+  
