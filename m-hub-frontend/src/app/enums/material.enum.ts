ff42d49ec66025ea21dfb6d92163959bafdaee32
Rasakul13 <39884629+Rasakul13@users.noreply.github.com>
2025-09-24 12:01:29 +0200

    feat: add material list as enum

--- m-hub-frontend/src/app/enums/material.enum.ts
+++ m-hub-frontend/src/app/enums/material.enum.ts
@@ -3,7 +3,7 @@
  */
 export enum Material {
   mat_1 = 'Ziegel',
-  mat_2 = 'mat_örtel',
+  mat_2 = 'Mörtel',
   mat_3 = 'Beton',
   mat_4 = 'Blähbeton',
   mat_5 = 'Estrich',
@@ -14,14 +14,14 @@ export enum Material {
   mat_10 = 'Fliesenkleber',
   mat_11 = 'Rigips',
   mat_12 = 'Holz',
-  mat_13 = 'Keramat_ik',
-  mat_14 = 'Bitumat_en',
+  mat_13 = 'Keramik',
+  mat_14 = 'Bitumen',
   mat_15 = 'Glas',
-  mat_16 = 'mat_ineralwolle',
+  mat_16 = 'Mineralwolle',
   mat_17 = 'Styropor',
   mat_18 = 'PVC',
   mat_19 = 'Kupfer',
-  mat_20 = 'Alumat_iniumat_',
+  mat_20 = 'Aluminium',
   mat_21 = 'Eternit',
   mat_22 = 'Schlacke',
   mat_23 = 'Blei',
@@ -29,8 +29,8 @@ export enum Material {
   mat_25 = 'Stroh',
   mat_26 = 'Naturstein',
   mat_27 = 'Papier',
-  mat_28 = 'mat_essing',
+  mat_28 = 'Messing',
   mat_29 = 'Steinzeug',
-  mat_30 = 'Linoleumat_'
+  mat_30 = 'Linoleum'
 } 
   
