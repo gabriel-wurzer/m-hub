ad7c75d7142f0595d9ba80ce9be5190a2d1edbe8
Rasakul13 <39884629+Rasakul13@users.noreply.github.com>
2026-02-17 16:10:51 +0100

    add template dialog for adding Bauteil

---
+++ m-hub-frontend/src/app/components/dialogs/add-part-dialog/add-part-dialog.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddPartDialogComponent } from './add-part-dialog.component';
+
+describe('AddPartDialogComponent', () => {
+  let component: AddPartDialogComponent;
+  let fixture: ComponentFixture<AddPartDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AddPartDialogComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AddPartDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ m-hub-frontend/src/app/components/dialogs/add-part-dialog/add-part-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-part-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-part-dialog.component.html',
+  styleUrl: './add-part-dialog.component.scss'
+})
+export class AddPartDialogComponent {
+
+}
