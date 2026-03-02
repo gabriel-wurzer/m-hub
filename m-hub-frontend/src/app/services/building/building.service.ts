52166c83de762a5afc09fe0a5786bfb885ae9cae
Rasakul13 <39884629+Rasakul13@users.noreply.github.com>
2026-03-02 12:37:51 +0100

    feat: add frontend service api for strcuture request

--- m-hub-frontend/src/app/services/building/building.service.ts
+++ m-hub-frontend/src/app/services/building/building.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 
 import { Building } from '../../models/building';
 import { BuildingComponent } from '../../models/building-component';
+import { Floor } from '../../models/floor';
 
 @Injectable({
   providedIn: 'root'
@@ -15,11 +16,25 @@ export class BuildingService {
   constructor(private http: HttpClient) { }
 
   // --- Base building ---
+
+    /**
+   * GET /api/buildings/:buildingId
+   */
   getBuildingById(buildingId: string): Observable<Building> {
     return this.http.get<Building>(`${this.buildingsUrl}/${buildingId}`);
   } 
 
+    /**
+   * GET /api/buildings/:buildingId/components
+   */ 
   getAllComponentsByBuilding(buildingId: string): Observable<BuildingComponent[]> {
     return this.http.get<BuildingComponent[]>(`${this.buildingsUrl}/${buildingId}/components`);
+  }
+
+    /**
+   * GET /api/buildings/:buildingId/latest-structure
+   */
+  getLatestBuildingStructure(buildingId: string): Observable<Floor[]> {
+    return this.http.get<Floor[]>(`${this.buildingsUrl}/${buildingId}/latest-structure`); 
   }
 }
