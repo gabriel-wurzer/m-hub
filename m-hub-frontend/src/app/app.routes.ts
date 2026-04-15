0354206141d3c337dcec0c8b41038d40959b82fe
Rasakul13 <39884629+Rasakul13@users.noreply.github.com>
2026-04-15 18:38:03 +0200

    feat: add imprint route and empty component

--- m-hub-frontend/src/app/app.routes.ts
+++ m-hub-frontend/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { UserDataComponent } from './components/user-data/user-data.component';
 import { MarketComponent } from './components/market/market.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { authenticationGuard } from './services/authentication/authentication.guard';
+import { ImprintComponent } from './components/imprint/imprint.component';
 
 export const routes: Routes = [
     { path: 'map', component: MapComponent },
     { path: 'market', component: MarketComponent },
     { path: 'data', component: UserDataComponent, canActivate: [authenticationGuard] },
+    { path: 'imprint', component: ImprintComponent },
     { path: '', redirectTo: 'map', pathMatch: 'full' },   // default route
     { path: '**', component: NotFoundComponent }          // wildcard route
   ];
