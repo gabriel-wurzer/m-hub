b75a7c5bbc57912cf5454d24797ce485e541f577
Gabriel Wurzer <gabriel.wurzer@tuwien.ac.at>
2026-07-24 15:17:39 +0200

    frontend: track material + material-group enums (von getrackten Komponenten importiert, waren untracked -> Checkout kaputt)

---
+++ m-hub-frontend/src/app/enums/material-group.enum.ts
@@ -0,0 +1,16 @@
+/**
+ * Enum for supported material groups.
+ */
+
+export enum MaterialGroup {
+    mg_1 = 'Beton inkl. Estrich',
+    mg_2 = 'Ziegelmauerwerk,',
+    mg_3 = 'Holz',
+    mg_4 = 'Stahl',
+    mg_5 = 'Glas',
+    mg_6 = 'Mineralwolle',
+    mg_7 = 'Polystyrol',
+    mg_8 = 'Gipsplatten, Gips',
+    mg_9 = 'Sand, Kies, Stein',
+  } 
+  
---
+++ m-hub-frontend/src/app/enums/material.enum.ts
@@ -0,0 +1,36 @@
+/**
+ * Enum for supported mataterials.
+ */
+export enum Material {
+  mat_1 = 'Ziegel',
+  mat_2 = 'Mörtel',
+  mat_3 = 'Beton',
+  mat_4 = 'Blähbeton',
+  mat_5 = 'Estrich',
+  mat_6 = 'Putz',
+  mat_7 = 'Stahl',
+  mat_8 = 'Heraklith',
+  mat_9 = 'Fliesen',
+  mat_10 = 'Fliesenkleber',
+  mat_11 = 'Rigips',
+  mat_12 = 'Holz',
+  mat_13 = 'Keramik',
+  mat_14 = 'Bitumen',
+  mat_15 = 'Glas',
+  mat_16 = 'Mineralwolle',
+  mat_17 = 'Styropor',
+  mat_18 = 'PVC',
+  mat_19 = 'Kupfer',
+  mat_20 = 'Aluminium',
+  mat_21 = 'Eternit',
+  mat_22 = 'Schlacke',
+  mat_23 = 'Blei',
+  mat_24 = 'Diverse Kunststoffe',
+  mat_25 = 'Stroh',
+  mat_26 = 'Naturstein',
+  mat_27 = 'Papier',
+  mat_28 = 'Messing',
+  mat_29 = 'Steinzeug',
+  mat_30 = 'Linoleum'
+} 
+  
